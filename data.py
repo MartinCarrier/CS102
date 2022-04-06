7e0dfc878d513eb0a1737472e7377f9e66bb6875
Martin Carrier <t894031@corp.ads>
2022-04-06 14:28:47 -0400

    first data

---
+++ data.py
@@ -0,0 +1,8 @@
+lego = ['Taj Mahal', 'Singapour', 'The White House', 'Batmobile', 'Infinit Saga', 'Spider-Man against Dr. Oct',
+        'Thanos', 'Avengers', 'T-Rex', 'Dr. Wu lab', "Luke's jet", 'Obi-Wan hut', 'Yoda', 'Destroyer', 'The Globe',
+        'Classic', 'World map', 'TB-TT', 'Death Star', 'Caravan', 'Camping', 'Hogwart', 'Phoenix', 'Medical visit',
+        'Titanic', 'Empire State Building', 'Paris', '90 years', 'Artic Explorer', 'Artic Slider']
+
+for item in lego:
+    print(item)
+
