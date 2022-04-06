dcbaf425f6861e4cc975832fbbf820f31fdd8db8
Martin Carrier <t894031@corp.ads>
2022-04-06 15:34:56 -0400

    data completed

--- data.py
+++ data.py
@@ -3,6 +3,40 @@ lego = ['Taj Mahal', 'Singapour', 'The White House', 'Batmobile', 'Infinit Saga'
         'Classic', 'World map', 'TB-TT', 'Death Star', 'Caravan', 'Camping', 'Hogwart', 'Phoenix', 'Medical visit',
         'Titanic', 'Empire State Building', 'Paris', '90 years', 'Artic Explorer', 'Artic Slider']
 
-for item in lego:
-    print(item)
+lego_data = [['18+',21056,'Architecture',2022,150,["Toy's Rus",'Wall-Mart','Amazon','Costco']],
+             ['18+',21057,'Architecture',827,75,["Toy's Rus",'Wall-Mart','Amazon']],
+             ['18+',21054,'Architecture',1483,150,['Archambeault',"Toy's Rus",'Wall-Mart','Amazon','Renaud-Bray']],
+             ['7+',76119,"Super-Hero",342,40,["Toy's Rus",'Amazon','Canadian Tire']],
+             ['8+',76192,'Super-Hero',527,110,["Toy's Rus",'Wall-Mart','Amazon','Best-Buy',"Costco"]],
+             ['6+',76148,'Super-Hero',234,25,["Toy's Rus",'Wall-Mart','Amazon','Canadian Tire']],
+             ['6+',76141,'Super-Hero',152,14,["Toy's Rus",'Wall-Mart',"Amazon",'Canadian Tire','Best-Buy']],
+             ['8+',76144,'Super-Hero',482,80,["Toy's Rus",'Wall-Mart','Amazon']],
+             ['7+',76940,'Dynausor',198,40,["Toy's Rus",'Wall-Mart']],
+             ['6+',75939,'Dynausor',164,30,["Toy's Rus",'Wall-Mart','Amazon','Canadian Tire']],
+             ['9+',75301,'Star Wars',474,70,["Toy's Rus",'Wall-Mart','Amazon','Canadian Tire']],
+             ['7+',75270,'Star Wars',200,40,["Toy's Rus",'Wall-Mart','Amazon','Canadian Tire']],
+             ['10+',75255,'Star Wars',1771,115,["Toy's Rus",'Wall-Mart','Amazon']],
+             ['6+',75252,'Star Wars',4784,850,["Toy's Rus",'Amazon','Wall-Mart']],
+             ['18+',21332,'Ideas',2585,270,['Amazon']],
+             ['4+',10698,'Creativ',790,47,["Toy's Rus",'Wall-Mart','Amazon']],
+             ['18+',31203,'Ideas',11695,350,['Best-Buy','Wall-Mart','Amazon']],
+             ['18+',75313,'Star Wars',6785,960,["Toy's Rus",'Amazon']],
+             ['14+',75159,'Star Wars',4016,600,['Best-Buy','Wall-Mart','Amazon']],
+             ['7+',41681,'Friends',487,70,["Toy's Rus",'Wall-Mart','Canadian Tire']],
+             ['6+',41392,'Friends',241,40,["Toy's Rus",'Wall-Mart','Canadian Tire','Amazon']],
+             ['18+',76391,'Harry Potter',3010,350,['Costco',"Toy's Rus",'Amazon']],
+             ['10+',76394,'Harry Potter',597,76,['Amazon']],
+             ['2+',10968,'Duplo',34,20,["Toy's Rus"]],
+             ['18+',10294,'Creator',9090,800,['Amazon']],
+             ['16+',21046,'Architecture',1767,245,['Amazon',"Toy's Rus",'Wall-Mart']],
+             ['12+',21044,'Architecture',649,70,['Amazon',"Toy's Rus",'Wall-Mart','Costco']],
+             ['5+',11021,'Creativ',1100,70,['Amazon']],
+             ['5+',60191,'CITY',70,13,['Amazon',"Toy's Rus",'Wall-Mart']],
+             ['5+',60190,'CITY',50,10,['Amazon',"Toy's Rus",'Wall-Mart','Renaud-Bray']]]
+
+#idx = 0
+#for name in lego:
+#    print("Lego '{0}' is part of '{1}' category".format(name, lego_data[idx][2]))
+#    idx += 1
+
 
